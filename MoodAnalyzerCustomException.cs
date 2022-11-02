ea7061101e7cb192c5297bc65d446de88e4aca57
jayukasode23 <jayukasode2398@gmail.com>
2022-11-02 20:52:41 +0530

    Added Invalid Mood

---
+++ MoodAnalyzerCustomException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserComputation
+{
+    public class MoodAnalyzerCustomException:Exception
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_FIELD,
+            NO_SUCH_METHOD,
+            NO_SUCH_CLASS,
+            OBJECT_CREATION_ISSUE
+        }
+        //Creating 'type' variable of type Exceptiontype
+        private readonly ExceptionType type;
+        
+        /// Parameterized constructor sets the Exception Type and message.
+        
+        /// <param name="Type"></param>
+        /// <param name="message"></param>
+        public MoodAnalyzerCustomException(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
+    
