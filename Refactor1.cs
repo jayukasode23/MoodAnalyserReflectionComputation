67f5b3a2dfe2289c753e2d46743e9a5f11f22312
jayukasode23 <jayukasode2398@gmail.com>
2022-11-02 20:43:54 +0530

    added Refactor 1.1 And 1.2

---
+++ Refactor1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserComputation
+{
+    public class Refactor1
+    {
+        public string message;
+        public Refactor1(string message)
+        {
+            this.message = message;
+        }
+        //Analysing the mood by passing happy Or sad.
+        public string AnalyseMood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
