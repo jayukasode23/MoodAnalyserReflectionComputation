acb211e555ba67a8ea7aac5d4ed90182fcdd146a
jayukasode23 <jayukasode2398@gmail.com>
2022-11-02 20:35:16 +0530

    Added Mood Analyser

---
+++ MoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserComputation
+{
+    public class MoodAnalyser
+    {
+        //Analysing the mood by passing happy Or sad.
+        public string AnalyseMood(string message)
+        {
+            if (message.Contains("Sad"))
+            {
+                return "Sad";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
+
